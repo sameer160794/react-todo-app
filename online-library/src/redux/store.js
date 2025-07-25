3080f2112287c6217505acd3a1ed6347f525c090
Sameer Qureshi <sameerqureshi160794@gmail.com>
2025-07-26 00:12:18 +0530

    Delete online-library directory

--- online-library/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-
-import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from './booksSlice';
-
-export const store = configureStore({
-  reducer: {
-    books: booksReducer,
-  },
-});
